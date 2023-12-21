59b96cd1965180042206440620481ece7d82e4f9
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-21 18:22:07 +0100

    inserite props su component FilmCard e SeriesCard

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,8 +16,12 @@
         <p>lingua: {{ series.original_language }}</p>
         <p>voto:{{ roundToFive(parseInt(series.vote_average)) }}/5</p>
     </div>
-    <FilmCard :requestedFilms="requestedFilms" />
-    <SeriesCard :requestedTvSeries="requestedTvSeries" />
+    <FilmCard v-for="film in requestedFilms" :title="film.title" :poster_path="film.poster_path"
+        :original_title="film.original_title" :original_language="film.original_language"
+        :vote_average="film.vote_average" />
+    <SeriesCard v-for="series in requestedTvSeries" :name="series.name" :poster_path="series.poster_path"
+        :original_name="series.original_name" :original_language="series.original_language"
+        :vote_average="series.vote_average" />
 </template>
 
 <script>
--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-
+    <div v-for="film in requestedFilms">
+        <img :src="`https://image.tmdb.org/t/p/w185/${film.poster_path}`" :alt="`${film.title} cover`">
+        <p>titolo: {{ film.title }}</p>
+        <p>titolo originale: {{ film.original_title }}</p>
+        <p>lingua: {{ film.original_language.toUpperCase() }}</p>
+        <!-- <img :src="`../assets/img/png100px/${film.original_language}.png`" alt="">
+        <img src="../assets/img/png100px/it.png" alt=""> -->
+        <img :src="`/src/assets/img/png100px/${film.original_language}.png`" alt="">
+        <p>voto: {{ roundToFive(parseInt(film.vote_average)) }} /5</p>
     </div>
 </template>
 
