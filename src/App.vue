be86ea6176426befb2ba7af82e07d830f534ec14
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-21 16:24:18 +0100

    aggiunta chiamata axios per serie TV

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,10 @@ export default {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2QxYjFmNjliZmFlNWQxZTE4MzQ0NmVmZWZiMTA5YyIsInN1YiI6IjY1ODMwNTYwZTI5NWI0M2JjNzY4NzExNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lZNNGkfj6G0PG7Dkrowbcmo9dtKA-R4srQlPpR2-YKI'
         },
       },
-      urlTv: {
+      urlTvSeries: {
         method: 'GET',
         url: 'https://api.themoviedb.org/3/search/tv',
-        params: { query: 'scrubs', language: 'en-US' },
+        params: { query: '', language: 'en-US' },
         headers: {
           accept: 'application/json',
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2QxYjFmNjliZmFlNWQxZTE4MzQ0NmVmZWZiMTA5YyIsInN1YiI6IjY1ODMwNTYwZTI5NWI0M2JjNzY4NzExNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lZNNGkfj6G0PG7Dkrowbcmo9dtKA-R4srQlPpR2-YKI'
@@ -36,14 +36,25 @@ export default {
     }
   },
   methods: {
-    defineSearch(film) {
-      this.urlMovie.params.query = film
+    defineSearch(searched) {
+      this.urlMovie.params.query = searched
+      this.urlTvSeries.params.query = searched
       this.axiosRequestMovie()
     },
     axiosRequestMovie() {
       axios.request(this.urlMovie)
         .then((response) => {
           this.requestedFilms = response.data.results
+          console.log(response.data.results);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    },
+    axiosRequestTvSeries() {
+      axios.request(this.urlTvSeries)
+        .then((response) => {
+          this.requestedTvSeries = response.data.results
           console.log(response.data.results);
         })
         .catch(function (error) {
