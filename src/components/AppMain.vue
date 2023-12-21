0a94c58f95c2e0df2fbb9e1744306fc2b19f8de5
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-21 17:39:20 +0100

    commentato problema

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,10 +3,10 @@
         <img :src="`https://image.tmdb.org/t/p/w185/${film.poster_path}`" :alt="`${film.title} cover`">
         <p>titolo: {{ film.title }}</p>
         <p>titolo originale: {{ film.original_title }}</p>
-        <p>lingua: {{ film.original_language }}</p>
-        <!-- `../assets/img/png100px/${film.original_language}.png` -->
-        <img :src="`../assets/img/png100px/${film.original_language}.png`" alt="">
-        <img src="../assets/img/png100px/it.png" alt="">
+        <p>lingua: {{ film.original_language.toUpperCase() }}</p>
+        <!-- <img :src="`../assets/img/png100px/${film.original_language}.png`" alt="">
+        <img src="../assets/img/png100px/it.png" alt=""> -->
+        <img :src="`/src/assets/img/png100px/${film.original_language}.png`" alt="">
         <p>voto: {{ film.vote_average }}/10 oppure {{ roundToFive(parseInt(film.vote_average)) }} /5</p>
     </div>
     <div v-for="series in requestedTvSeries">
