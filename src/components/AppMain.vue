df2e8047cc986845076eaca08104ff639c014a7d
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-21 16:37:56 +0100

    inserito contenuto in v-for per array serie tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,7 +6,13 @@
         <p>lingua: {{ film.original_language }}</p>
         <p>voto: {{ film.vote_average }}/10 oppure {{ roundToFive(parseInt(film.vote_average)) }} /5</p>
     </div>
-    <div v-for="series in requestedTvSeries">serie rilevata</div>
+    <div v-for="series in requestedTvSeries">
+        <img :src="`https://image.tmdb.org/t/p/w185/${series.poster_path}`" :alt="`${series.name} cover`">
+        <p>titolo: {{ series.name }}</p>
+        <p>titolo originale: {{ series.original_name }}</p>
+        <p>lingua: {{ series.original_language }}</p>
+        <p>voto: {{ series.vote_average }}/10 oppure {{ roundToFive(parseInt(series.vote_average)) }} /5</p>
+    </div>
 </template>
 
 <script>
