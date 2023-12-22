afe0368e8a10f90c6380e51bdc17972b301c2fce
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-22 10:01:05 +0100

    inserito logo, ul e ordinato AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,22 @@
 <template>
     <header>
-        <input type="text" v-model="lookedFor" @keyup.enter="$emit('movieSearched', lookedFor)">
-        <button @click="$emit('movieSearched', lookedFor)"> Cerca! </button>
+        <div class="logo-container">
+            <img src="../assets/img/netflix_logo.png" alt="Netflix Logo">
+        </div>
+        <nav>
+            <ul class="d-flex m-0 p-0">
+                <li class="mx-2">TV Shows</li>
+                <li class="mx-2">Movies</li>
+                <li class="mx-2">Recently Added</li>
+                <li class="mx-2">My List</li>
+            </ul>
+            <ul class="d-flex  m-0 me-3 p-0">
+                <input type="text" v-model="lookedFor" @keyup.enter="$emit('movieSearched', lookedFor)">
+                <button @click="$emit('movieSearched', lookedFor)"> Cerca! </button>
+            </ul>
+
+        </nav>
+
     </header>
 </template>
 
@@ -23,6 +38,24 @@ export default {
 header {
     height: 15vh;
     background-color: $bg-header;
+
+    @include flex(row, start, center);
+
+    div.logo-container {
+        width: 15%;
+
+        img {
+            width: calc(100% - 1.8rem);
+            margin: 0 0.9rem 0 0.9rem;
+        }
+    }
+
+    nav {
+        @include flex(row, space-between, center);
+        width: 85%;
+        height: 100%;
+        color: white;
+    }
 
 }
 </style>
